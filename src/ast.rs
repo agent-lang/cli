3ce753177ffeffd47346e0b8555ccdda68c4e004
5eqn <491100866@qq.com>
2024-03-31 16:23:41 +0800

    feat: hard coded cli

--- src/ast.rs
+++ src/ast.rs
@@ -1,120 +1,83 @@
 use std::fmt::Display;
 
-/// Param = name: Type /* Description */
-#[derive(Debug, Clone)]
-pub struct Param(pub String, pub String, pub Box<Type>);
+pub trait Term<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result;
+    fn eval(&self) -> T;
+}
 
-/// Param can be displayed
-impl Display for Param {
+impl<T> Display for dyn Term<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}: {} /* {} */", self.0, self.2, self.1)
+        self.fmt(f)
     }
 }
 
-/// Type describes the property an operation satisfies
-#[derive(Debug, Clone)]
-pub enum Type {
-    /// Function: param -> ReturnType
-    Func(Param, Box<Type>),
+pub struct Lit(String);
+pub struct Predict(Box<dyn Term<String>>);
+pub struct Ask(Box<dyn Term<String>>);
+pub struct Code(Box<dyn Term<String>>, Box<dyn Term<String>>);
+pub struct Hole;
 
-    /// Exact is type literal
-    Exact(String),
-}
-
-/// Type can be displayed
-impl Display for Type {
+impl Term<String> for Lit {
+    fn eval(&self) -> String {
+        let Lit(id) = self;
+        id.to_string()
+    }
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Type::Func(param, ty) => write!(f, "({}) -> ({})", param, ty),
-            Type::Exact(id) => write!(f, "{}", id),
-        }
+        let Lit(id) = self;
+        write!(f, "{}", id)
     }
 }
 
-/// Term describes what an operation is
-#[derive(Debug, Clone)]
-pub enum Term {
-    /// Function: param => returnTerm
-    Func(Param, Box<Term>),
-
-    /// Variable
-    Var(String),
-
-    /// Literal
-    Lit(String),
-
-    /// Application: func arg
-    App(Box<Term>, Box<Term>),
-}
-
-/// Term can be displayed
-impl Display for Term {
+impl Term<String> for Predict {
+    fn eval(&self) -> String {
+        let Predict(prefix) = self;
+        format!("predict({})", prefix.eval())
+    }
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Term::Func(param, term) => write!(f, "({}) => ({})", param, term),
-            Term::Var(id) => write!(f, "{}", id),
-            Term::Lit(lit) => write!(f, "\"{}\"", lit),
-            Term::App(func, arg) => write!(f, "({})({})", func, arg),
-        }
+        let Predict(prefix) = self;
+        write!(
+            f,
+            "/* The next predicted words of `prefix` */ predict(prefix = {})",
+            prefix
+        )
     }
 }
 
-/// Term can be evaluated to value
-impl Term {
-    pub fn eval(self, env: &Env) -> Val {
-        match self {
-            Term::Func(param, next) => Val::Func(param, next, env.clone()),
-            Term::Var(id) => match env.iter().find(|v| v.0 == id) {
-                Some(val) => val.1.clone(),
-                None => panic!("variable not found"),
-            },
-            Term::Lit(lit) => Val::Lit(lit),
-            Term::App(func, arg) => {
-                let eval = func.eval(env);
-                eval.apply(arg.eval(env))
-            }
-        }
+impl Term<String> for Ask {
+    fn eval(&self) -> String {
+        let Ask(question) = self;
+        format!("ask({})", question.eval())
+    }
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Ask(question) = self;
+        write!(
+            f,
+            "/* User's answer to `question` */ ask(question = {})",
+            question
+        )
     }
 }
 
-/// Value is the semantics of a term
-#[derive(Debug, Clone)]
-pub enum Val {
-    /// Function encloses a term with its environment
-    Func(Param, Box<Term>, Env),
-
-    /// Variable
-    Var(String),
-
-    /// Literal
-    Lit(String),
-
-    /// Application: func arg
-    App(Box<Val>, Box<Val>),
-}
-
-/// Value can be applied with argument
-impl Val {
-    fn apply(self, arg: Val) -> Val {
-        match self {
-            Val::Func(param, next, mut env) => {
-                // Push argument to environment
-                env.push(Arg(param.0, arg));
-
-                // Evaluate with complete environment
-                next.eval(&env)
-            }
-            val => Val::App(Box::new(val), Box::new(arg)),
-        }
+impl Term<String> for Code {
+    fn eval(&self) -> String {
+        let Code(language, description) = self;
+        format!("code({}, {})", language.eval(), description.eval())
+    }
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Code(language, description) = self;
+        write!(
+            f,
+            "/* The code in `language` according to `description` */ code(language = {}, description = {})",
+            language, description
+        )
     }
 }
 
-/// Context stores the mapping from name to type
-pub type Context = Vec<Param>;
-
-/// Env stores the mapping from name to value
-pub type Env = Vec<Arg>;
-
-/// Argument: name = value
-#[derive(Clone, Debug)]
-pub struct Arg(pub String, pub Val);
+impl Term<String> for Hole {
+    fn eval(&self) -> String {
+        format!("_")
+    }
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "<placeholder>")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,5 @@
-use ast::Term;
-use run::run;
-
-use crate::ast::{Arg, Val};
-
 mod api;
 mod ast;
 mod gen;
-mod run;
 
-fn main() {
-    test();
-}
-
-async fn test() {
-    let term = Term::App(
-        Box::new(Term::Var("predict".to_string())),
-        Box::new(Term::Lit("123".to_string())),
-    );
-    let val = term.eval(&vec![Arg(
-        "predict".to_string(),
-        Val::Var("predict".to_string()),
-    )]);
-    let res = run(val).await;
-    println!("result: {}", res);
-}
+fn main() {}
